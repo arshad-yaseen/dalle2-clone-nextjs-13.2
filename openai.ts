bfd34dd4b9e34c111cadcdd0ebcbd61cc4df731a
Glowit Labs <glowitsofware@gmail.com>
2023-03-31 09:22:04 +0530

    api key changed

--- openai.ts
+++ openai.ts
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai"
 
 const config = new Configuration({
     organization: process.env.NEXT_PUBLIC_OPENAI_ORGANIZATION_ID,
-    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_ORGANIZATION_ID
 })
 
 const openai = new OpenAIApi(config)
